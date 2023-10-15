8751dafb4fe53e58f55fa4813762631e3f25ed72
suresh-gundapu <suresh.gundapu2021@gmail.com>
2023-10-15 17:06:13 +0530

    Counter App: latest changes

--- src/Task1.js
+++ src/Task1.js
@@ -1,13 +1,42 @@
-import React from "react";
-
+import React, { useState } from "react";
 const Task1 = () => {
+    let[count , setCount] = useState(0);
 
+let handleCounterupdate = ()=>{
+    setCount((prevState)=>{
+        return count+1;
+    });
+}
     return (
 
-        <div className="text-center text-danger">
-            <h1>
-                Basic Example Of useState - Counter App
-            </h1>
+        <div className="text-center">
+
+            <div className="card shadow my-2">
+                <div className="card-header">
+                    <h1 className="text-danger">
+                        Basic Example Of useState - Counter App
+                    </h1>
+                </div>
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-md-12 col-lg-12 col-sm-12">
+                                {count}
+                        </div>
+
+                    </div>
+
+                    <div className="row my-2">
+                        <div className="col-md-12 col-lg-12 col-sm-12">
+                            <button className="btn btn-primary" onClick={()=>{
+                                handleCounterupdate()
+                            }}>
+                                Counter Update
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
