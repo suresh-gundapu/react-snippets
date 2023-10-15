61e4f7536e1838fd46c42ce9f664640db7c1f1a6
suresh-gundapu <suresh.gundapu2021@gmail.com>
2023-10-15 15:27:02 +0530

    Basic Example of useState:Counter App

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Task1 from "./Task1";
+function App() {
+  return (
+    <>
+      <Task1 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Task1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Task1 = () => {
+
+    return (
+
+        <div className="text-center text-danger">
+            <h1>
+                Basic Example Of useState - Counter App
+            </h1>
+        </div>
+    )
+}
+
+export default Task1;
