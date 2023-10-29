191f16e6a75f5fffc973ff8bc0278547f6255613
suresh-gundapu <suresh.gundapu2021@gmail.com>
2023-10-29 18:15:22 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import Task1 from "./Task1";
-import Task3 from "./Task3";
-import Task4 from "./Task4";
-import Task2 from "./Task2";
-import Task6 from "./Task6";
-import Task7 from "./Task7";
+import React from "react";
+
+import { Routes, Route } from "react-router-dom";
+import Task1 from "./ReactCore/Task1";
+import Task3 from "./ReactCore/Task3";
+import Task4 from "./ReactCore/Task4";
+import Task2 from "./ReactCore/Task2";
+import Task6 from "./ReactCore/Task6";
+import Task7 from "./ReactCore/Task7";
+import Navbar from "./Navbar";
+import Home from "./ReactCore/Home";
 function App() {
   return (
     <>
-      <Task1 />
-      <Task3 />
-      <Task4 />
-      <Task2/>
-      <Task6/>
-      <Task7/>
+      <div className="container-fluid">
+        <div className="row">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} >
+              <Route path="task1" element={<Task1 />} />
+              <Route path="task3" element={<Task3 />} />
+              <Route path="task4" element={<Task4 />} />
+              <Route path="task6" element={<Task6 />} />
+              <Route path="task7" element={<Task7 />} />
+
+            </Route>
+          </Routes>
+        </div>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
-
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 );
 
